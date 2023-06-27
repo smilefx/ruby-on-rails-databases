7456657dc4804101f76707edee6dc107ae31beff
smilefx <SmileFX.LM@gmail.com>
2023-06-27 18:47:23 +0200

    Added Tasks

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,13 +1,35 @@
 class Course < ApplicationRecord
+    # Assoziation zu Events
     has_many :events, class_name: "Event", dependent: :delete_all
 
-    # Validations
+    # Validierungen
     validates :name, presence: { message: "Kursname muss angegeben werden." }
-    validates :semester, presence: { message: "Fachsemester muss angegeben werden." }, numericality: { only_integer: true, message: "Fachsemester muss eine Zahl sein." }
-    validates :year, presence: { message: "Semester muss angegeben werden." }, numericality: { only_integer: true, message: "Semester muss eine Jahreszahl sein." }, length: { is: 4, message: "Semester muss eine valides Jahr sein." }, inclusion: { in: 2000..2100, message: "Semester muss zwischen 2000 und 2100 liegen."}
-    validates :period, presence: true
-    validates :short_name, presence: { message: "Kürzel muss angegeben werden." }, length: { maximum: 10, message: "Kürzel darf maximal 10 Zeichen lang sein." }
 
+    validates :semester, presence: { message: "Fachsemester muss angegeben werden." }
+    validates :semester, numericality: { only_integer: true, message: "Fachsemester muss eine Zahl sein." }
+
+    validates :year, presence: { message: "Semester muss angegeben werden." }
+    validates :year, numericality: { only_integer: true, message: "Semester muss eine Jahreszahl sein." }
+    validates :year, length: { is: 4, message: "Semester muss eine valides Jahr sein." }
+    validates :year, inclusion: { in: 2000..2100, message: "Semester muss zwischen 2000 und 2100 liegen."}
+
+    validates :period, presence: true
+
+
+
+
+    # =====  TODO - Beispielaufgabe 3  =====
+    #      
+    # · Ergänze das Kürzel um eine Validierung, damit dieses maximal 10 Zeichen lang sein darf
+    # · Füge beiden Validierungen eine eigene Fehlermeldung hinzu, die bei einem Fehler angezeigt wird
+
+    validates :short_name, presence: true
+
+    # ======================================
+
+
+
+    # Methode für ausstehende Events
     def upcoming
         self.events.where("date >= ?", Date.today).order(date: :asc, time: :asc)
     end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,24 +1,45 @@
 class Event < ApplicationRecord
-  belongs_to :course
+    belongs_to :course
 
-  # Validations
-  VALID_STATUSES = ['Ausstehend', 'In Arbeit', 'Erledigt']
-  validates :status, inclusion: { in: VALID_STATUSES }
-  validates :title, presence: { message: "Titel muss angegeben werden." }
-  validates :date, presence: { message: "Datum muss angegeben werden." }
-  validates :time, presence: { message: "Uhrzeit muss angegeben werden." }
+    # Validations
+    VALID_STATUSES = ['Ausstehend', 'In Arbeit', 'Erledigt']
+    validates :status, inclusion: { in: VALID_STATUSES }
 
-  default_scope { order(date: :asc, time: :asc) }
+    validates :title, presence: { message: "Titel muss angegeben werden." }
 
-  def named_date
-    I18n.l(self.date, format: "%d. %B %Y")
-  end
+    validates :date, presence: { message: "Datum muss angegeben werden." }
 
-  def named_date_day
-    I18n.l(self.date, format: "%A, %d. %B %Y")
-  end
+    validates :time, presence: { message: "Uhrzeit muss angegeben werden." }
 
-  def named_time
-    I18n.l(self.time, format: "%H:%M Uhr")
-  end
+    default_scope { order(date: :asc, time: :asc) }
+
+
+
+
+    # =====  TODO - Beispielaufgabe 4  =====
+    #      
+    # · Schreibe eine Methode, die das Datum im Format "01. Januar 2024" zurückgibt
+    # · I18n.l() kann dabei helfen, das Datum in das gewünschte Format zu bringen (https://api.rubyonrails.org/classes/DateTime.html#method-i-strftime)
+    #   · %A = Wochentag
+    #   · %d = Tag
+    #   · %B = Monat
+    #   · %Y = Jahr
+    # · Ersetze dafür alles innerhalb der Methode "named_date"
+
+    def named_date
+        self.date.to_s
+    end
+
+    # ======================================
+
+
+
+
+    def named_date_day
+        I18n.l(self.date, format: "%A, ") + self.named_date 
+    end
+
+    def named_time
+        I18n.l(self.time, format: "%H:%M Uhr")
+    end
 end
